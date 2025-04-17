938634bff1c985816078873be40f686bbfea703d
Saurabh Raj <saurabhraj@jmangroup.com>
2025-04-17 18:26:47 +0530

    Fix: added missing / route for test

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\VideoController;
 
+Route::get('/', function () {
+    return response()->json(['message' => 'It works!']);
+});
+
 Route::get('/videos', [VideoController::class, 'index']);
 
 Route::get('/videos/search', [VideoController::class, 'search']);
